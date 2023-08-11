aa7954d8e063b77ee94819204ac0b7bd0784a73d
Todd W Crone <tcrone@newrelic.com>
2023-08-11 09:54:42 -0400

    12-4

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 3 {
+        println!("Usage: cargo run -- <query> <file_path>");
+        return
+    }
+
     let query = &args[1];
     let file_path = &args[2];
 
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
     println!("Searching for {}", query);
     println!("In file {}", file_path);
+    println!("With text:\n{contents}");
 }
