fd62fd14c6d794895a8e0bedfc8665033455ff5a
Todd W Crone <tcrone@newrelic.com>
2023-09-20 10:52:53 -0400

    13-17

--- src/lib.rs
+++ src/lib.rs
@@ -31,15 +31,23 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(
+        args: &[String]
+    ) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("Usage: cargo run -- <query> <file_path>");
+            return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config{ query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
